537da4bdfb44a1f77da46fbb2cba2967685e8183
Reyner Dittert <152820684+Gr1nns@users.noreply.github.com>
2025-06-24 21:44:04 -0300

    Update calculadora.cpp

--- calculadora.cpp
+++ calculadora.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<string>
+#include<math.h>
 using namespace std;
 int main(){
     string resp;
@@ -7,33 +8,76 @@ int main(){
     string cont = "S" ;
 
   while(cont=="S"){
-    cout<<"Escolha o que quer fazer: + (Adicao), - (Subtracao), * (Multiplicacao) ou / (Divisao): ";
+
+    cout<<"Escolha a acao que deseja realizar: ";
+    cout<<" + para adicao";
+    cout<<" - para subtracao";
+    cout<<" * para multiplicacao";
+    cout<<" / para divisao";
+    cout<<" ** para potenciacao";
+    cout<<" // para raiz quadrada";
+
     cin>>resp;
-    cout<<"Digite o primeiro valor: ";
+
+    if(resp == "+"){
+        cout<<"Digite o primeiro valor: ";
     cin>>n1;
     cout<<"Digite o segundo valor:";
     cin>>n2;
-
-    if(resp == "+"){
         n3 = n1 + n2;
         cout<<"A soma entre "<<n1<<" + "<<n2<<" = "<<n3<<endl;
         n3=0;
     }
+
     else if(resp == "-"){
+        cout<<"Digite o primeiro valor: ";
+    cin>>n1;
+    cout<<"Digite o segundo valor:";
+    cin>>n2;
         n3 = n1 - n2;
         cout<<"A subtracao entre "<<n1<<" - "<<n2<<" = "<<n3<<endl;
         n3=0;
     }
+
     else if(resp == "*"){
+        cout<<"Digite o primeiro valor: ";
+    cin>>n1;
+    cout<<"Digite o segundo valor:";
+    cin>>n2;
         n3 = n1 * n2;
         cout<<"A multiplicacao entre "<<n1<<" * "<<n2<<" = "<<n3<<endl;
         n3=0;
     }
+
     else if(resp == "/"){
+        cout<<"Digite o primeiro valor: ";
+          cin>>n1;
+        cout<<"Digite o segundo valor:";
+          cin>>n2;
         n3 = n1 / n2;
         cout<<"A divisao de "<<n1<<" / "<<n2<<" = "<<n3<<endl;
         n3=0;
     }
+
+    else if(resp == "//"){
+        cout<<"Digite o valor: ";
+          cin>>n1;
+    
+        n3 = sqrt(n1);
+        cout<<"A raiz de "<<n1<<" = "<<n3<<endl;
+        n3=0;
+    }
+
+     else if(resp == "**"){
+        cout<<"Digite o valor a ser potenciado: ";
+         cin>>n1;
+        cout<<"Digite o valor que potenciara:";
+         cin>>n2;
+        n3 = pow(n1,n2);
+        cout<<"A potenciacao de "<<n1<<" ** "<<n2<<" = "<<n3<<endl;
+        n3=0;
+    }
+   
     cout<<"Deseja continuar? S ou N:";
     cin>>cont;
   }
